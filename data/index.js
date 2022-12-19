7ddea8dcafaf76ba90f200cc49bd51679f2b2f5c
DeltaX8 <114773667+DeltaX8@users.noreply.github.com>
2022-12-19 09:23:01 -0600

    wrote script for home/landing page

--- data/index.js
+++ data/index.js
@@ -1,1 +1,12 @@
+// Scripted by DeltaX8
 
+var userInput = document.getElementById("assignmentInput").value;
+var code = "omega10x";
+
+function AddToList()
+{
+ if (userInput == code)
+ {
+  window.location.replace("data/main.html"); 
+ }
+}
